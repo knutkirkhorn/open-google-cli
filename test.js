00c0a94fe9f54167fff5aee059343b8eb9c67e31
Knut Kirkhorn <knutkirk@hotmail.com>
2023-02-20 20:19:09 +0100

    Fix `execa` importing

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 import test from 'ava';
-import execa from 'execa';
+import {execa} from 'execa';
 
 test('cli can print out', async t => {
 	const {stdout} = await execa('./cli.js', ['--version']);
